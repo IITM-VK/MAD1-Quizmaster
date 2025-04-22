e8d6ef6e946d8b42d2f7c85e9c58b18b0d1755a8
Vivek <vivekkumargrd04@gmail.com>
2025-04-22 21:37:55 +0530

    Fix: Modified app.py and .gitignore

--- app.py
+++ app.py
@@ -37,3 +37,8 @@ from seed_data import seed_database
 with app.app_context():
     upgrade()
     seed_database()
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
